33b09f8dabba079f3d681a5681ab0683dbcf014a
elliottsaruski <elliottxsaruski@gmail.com>
2026-06-02 17:50:15 -0400

    update soundcloud logo import

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
 import FooterTitle from "./components/FooterTitle";
 import WaveSurferComponent from "./components/WaveSurferComponent";
+import soundcloudLogo from "./assets/soundcloud-logo.png";
 
 function App() {
   return (
     <main>
       <a href="https://soundcloud.com/envimusic">
         <img
-          src="./src/assets/soundcloud-logo.png"
-          alt=""
+          src={soundcloudLogo}
+          alt="soundcloud logo"
           className="soundcloud-logo"
         />
       </a>
